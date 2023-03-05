86d12c9defc41848a20b7c60a3153803905d7727
DuncanIreri <duncanireri@gmail.com>
2023-03-05 23:31:44 +0300

    added oh my zsh configs

--- grimm-apps.sh
+++ grimm-apps.sh
@@ -261,6 +261,18 @@ sudo freshclam
 systemctl start clamav-freshclam
 
 echo
+tput setaf 2
+echo "################################################################"
+echo "################### Setting Up Zsh"
+echo "################################################################"
+tput sgr0
+echo
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+echo
 tput setaf 6
 echo "################################################################"
 echo "################### Done"
